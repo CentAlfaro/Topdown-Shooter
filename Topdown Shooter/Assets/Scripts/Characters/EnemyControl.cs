22c61eb1767a0103f924b924b6f2baf785b5b44a
CentAlfaro <vincent.alfaro28@gmail.com>
2022-03-25 11:19:24 +0800

    Added enemy player tracking

--- Topdown Shooter/Assets/Scripts/Characters/EnemyControl.cs
+++ Topdown Shooter/Assets/Scripts/Characters/EnemyControl.cs
@@ -1,17 +1,28 @@
+using UnityEngine;
+
 namespace Characters
 {
     public class EnemyControl : Character
     {
-        // Start is called before the first frame update
+        private GameObject _playerTracked;
+        
         public override void Start()
         {
-        
+            _playerTracked = GameObject.FindGameObjectWithTag("player");
         }
-
-        // Update is called once per frame
+        
         public override void Update()
         {
-        
+            PlayerTrack();
+        }
+
+        private void PlayerTrack()
+        {
+            var enemyTransform = transform;
+            var position = _playerTracked.transform.position;
+            enemyTransform.position = Vector2.MoveTowards(enemyTransform.position, position,
+                MovementSpeed * Time.deltaTime);
+            enemyTransform.up = position - enemyTransform.position;
         }
     }
 }
